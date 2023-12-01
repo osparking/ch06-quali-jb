838af4a33efdd3594fa03d501eaec10e832adfcb
osparking <jbpark03@gmail.com>
2023-12-01 22:10:43 +0900

    Vehicle 단순화 판 - Driver 공급됨

--- src/main/java/ch06/depend/Vehicle.java
+++ src/main/java/ch06/depend/Vehicle.java
@@ -1,8 +1,12 @@
 package ch06.depend;
 
 class Vehicle {
-	Driver d = new Driver();
+	Driver d;
 	boolean hasDriver = true;
+	
+	Vehicle(Driver d) {
+		this.d = d;
+	}
 
 	public void setHasDriver(boolean hasDriver) {
 		this.hasDriver = hasDriver;
