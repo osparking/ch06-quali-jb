d4db509c7a8d471cb7d8be4796766cc58a09db82
osparking <jbpark03@gmail.com>
2023-12-02 19:00:35 +0900

    형인자를 사용한 컴파일러 경고 제거

--- src/main/java/ch06/depend/StaticTarget.java
+++ src/main/java/ch06/depend/StaticTarget.java
@@ -4,8 +4,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class StaticTarget {
-	public static Set union(Set s1, Set s2) {
-		Set result = new HashSet(s1);
+	public static <E> Set<E> union(Set<E> s1, Set<E> s2) {
+		Set<E> result = new HashSet<>(s1);
 		result.addAll(s2);
 		return result;
 	}
