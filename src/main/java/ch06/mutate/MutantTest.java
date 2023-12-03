b905a98bd599d08dfdc35609b2ccfb884ef64df0
osparking <jbpark03@gmail.com>
2023-12-03 11:56:20 +0900

    변이 시험 - 원본

---
+++ src/main/java/ch06/mutate/MutantTest.java
@@ -0,0 +1,19 @@
+package ch06.mutate;
+
+public class MutantTest {
+	public static void main(String[] args) {
+		System.out.println(changeBeta(true));
+	}
+
+	private static int changeBeta(boolean a) {
+		int b = 0;
+		
+		if (a) {
+			b = 1;
+		} else {
+			b = 2;
+		}
+		
+		return b;
+	}
+}
